e662d87b7b4c079274e2b76580ae563add8cf08c
zfletch <zfletch2@gmail.com>
2016-02-08 17:05:36 -0500

    mvp

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,25 @@
+class NotesController < ApplicationController
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to action: :show, id: @note.reload.uuid
+    else
+      render :new
+    end
+  end
+
+  def show
+    @note = Note.find_by(uuid: params[:id])
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:text)
+  end
+end
